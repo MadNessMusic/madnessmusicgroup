645f704afd9e8e59fc576196c4c13651982c49e6
MadNessMusic <contact@madnessmusicgroup.com>
2026-03-19 01:57:23 -0600

    new

--- src/i18n/utils.ts
+++
@@ -1,8 +0,0 @@
-import { translations } from "./translations";
-
-type Lang = "es" | "en";
-type TranslationKey = keyof typeof translations;
-
-export function t(key: TranslationKey, lang: Lang = "es") {
-  return translations[key]?.[lang] || key;
-}
