e949fffbe7babd8af9c563ecc16df80988c07902
MadNessMusic <contact@madnessmusicgroup.com>
2026-03-23 22:11:59 -0600

    fix vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
 
-// https://astro.build/config
 export default defineConfig({
-  output: "server", // 🔑 ACTIVA SSR REAL
+  output: "server",
 
-  adapter: node({
-    mode: "standalone", // recomendado
-  }),
+  adapter: vercel(),
 
   vite: {
+    // @ts-ignore
     plugins: [tailwindcss()],
   },
 
