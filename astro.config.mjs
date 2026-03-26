3eda663121332c570950b155a360240e096b8d31
MadNessMusic <contact@madnessmusicgroup.com>
2026-03-26 00:36:39 -0600

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: "static",
+  output: "server", // 👈 IMPORTANTE
+  adapter: vercel(),
 
   vite: {
     // @ts-ignore
