92ae00e5927bfc6d0c23ef2e251bf1f6cd03ec03
MadNessMusic <contact@madnessmusicgroup.com>
2026-03-28 02:53:09 -0600

    fix?

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: "server", // 👈 IMPORTANTE
+  output: "static", // 👈 IMPORTANTE
   adapter: vercel(),
 
   vite: {
