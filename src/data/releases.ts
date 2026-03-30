0262eb293617b046b15e0cee9a5f2f2514762623
MadNessMusic <contact@madnessmusicgroup.com>
2026-03-30 17:43:47 -0600

    new

--- src/data/releases.ts
+++ src/data/releases.ts
@@ -11,6 +11,12 @@ export const releases: Release[] = [
     spotifyAlbumId: "5r2tgpw6u7UOFOkC3ee5WL",
     source: "madness",
     cover: "",
+  }
+  {
+    id: "release-baskiat-volver a empezar",
+    spotifyAlbumId: "0PGmnOC5erWJw8ii0s4WQG",
+    source: "supported",
+    cover: "",
   },
   {
     id: "release-rexx-legally",
