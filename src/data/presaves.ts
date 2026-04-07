f8e474ca03a23d5f28b803768a132bc4ed941aeb
MadNessMusic <contact@madnessmusicgroup.com>
2026-04-07 00:58:35 -0600

    new

--- src/data/presaves.ts
+++ src/data/presaves.ts
@@ -1,9 +1,17 @@
 export const presaves = [
   {
-    title: "t haces wey",
-    artist: "Feely, Astral Madafaka, Zavala Produce",
-    image: "/images/releases/coming-soon.png",
-    url: "https://hypeddit.com/feely/thaceswey",
+    title: "derretido en el fracaso",
+    artist: "telmar, astralmadafaka",
+    image: "/images/presaves/presave-derretidoenelfracaso.png",
+    url: "https://hypeddit.com/telmar/derretidoenelfracaso",
     source: "supported",
   },
+  {
+    title: "moshi moshi",
+    artist: "Oh leany, Rubirosa Playboy",
+    image: "/images/presaves/presave-moshimoshi.png",
+    url: "https://hypeddit.com/rubileany/moshimoshi",
+    source: "supported",
+  },
+
 ];
